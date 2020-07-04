579381e8372fbb0955c28c5a69c041c685bf40dc
KDSGYT <kdsgyt@gmail.com>
2020-07-04 16:53:32 -0400

    Added the card to display the results instead of display just the text

--- js/script.js
+++ js/script.js
@@ -1,40 +1,35 @@
 /**
  * you can store data in client cookies to store store history
  *  you can use that history to access the youtube channels
- * 
- * 
- * 
- * 
- * First take the keyword and get the username from there and use that to get the subs and views
- * look line 52 for the serach url
  */
+console.warn("Make sure you know what you are doing");
+console.warn("Made by KDSG (Karan Pal Singh) \n Search: KDSGYT (Google it )");
+
 const input = document.getElementById("username");
-const button = document.querySelector('.submit')
-const counts = document.querySelector('.subs');
+const button = document.querySelector(".submit");
+const counts = document.querySelector(".subs");
+const card = document.querySelector(".card");
+const channelProfileImage = card.querySelector(".card-circle");
 
 /**
  *  Fetch data from url if gets error then callback with another url
- * @param { string } url 
- * @param { function } callback 
- * @param { string } nxtUrl 
+ * @param { string } url
  */
-async function fetchAndCallback(url) {
-    const api = "AIzaSyDl5bov83fBW37QXinJvn4bS9cf-czCNxY";
-    await fetch(url)
-        .then(data => data.json())
-        .then(data => {
-            console.log(data);
-            try {
-                let item = data.items[0];
-                // console.log(item);
-                generateHTML(item);
-            } catch (err) {
-                throw new Error(err.message);
-            }
-        })
-        .catch(err => {
-            alert(err.message)
-        })
+async function fetchData(url) {
+  // const api = "AIzaSyDl5bov83fBW37QXinJvn4bS9cf-czCNxY";
+  await fetch(url)
+    .then((data) => data.json())
+    .then((data) => {
+      try {
+        let item = data.items[0];
+        generateHTML(item);
+      } catch (err) {
+        throw new Error("Not able to locate the channel");
+      }
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
 }
 
 /**
@@ -42,68 +37,76 @@ async function fetchAndCallback(url) {
  * @param { string } username as entered by the user
  */
 async function fetchDataByUsername(input = "pewdiepie") {
-    const api = "AIzaSyDl5bov83fBW37QXinJvn4bS9cf-czCNxY";
-    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${input}&key=${api}`
-    await fetch(url)
-        .then(data => data.json())
-        .then(data => data.items[0].id.channelId)
-        .then(async data => {
-            console.log(data);
-            let url = `https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${data}&key=${api}`;
-            await fetchAndCallback(url)
-        })
-        .catch(err => console.log(err.message))
+  const api = "AIzaSyC1UlcFjhhMckBnExrm8K1UyVMJyxgWT5c";
+  let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${input}&key=${api}`;
+  await fetch(url)
+    .then((data) => data.json())
+    .then((data) => data.items[0].id.channelId)
+    .then(async (data) => {
+      let url = `https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${data}&key=${api}`;
+      await fetchData(url);
+    })
+    .catch((err) => console.log(err.message));
 }
 /**
  * GenerateHTML: outputs html to the DOM using selectors.
  * @param { object } data data received from api
  */
 async function generateHTML(data) {
+  const thumbnails = data.snippet.thumbnails;
+  if (!data.message) {
+    if (window.screen.width >= 1024) updateImages(thumbnails.high.url);
     counts.innerHTML = `
         <span>
-            <span>${data.snippet.title}: <span>
-            Subscriber: <span>${shortNumber(data.statistics.subscriberCount)}</span>
-            Views: <span>${shortNumber(data.statistics.viewCount)}</span>
-        </span>`
+            <span class="channelTitle"><h2>${data.snippet.title}</h2> </span>
+            <div class="stats">
+                <span class="channelSubs">Subs: ${shortNumber(
+                  data.statistics.subscriberCount
+                )}</span>
+                <span class="channelViews"> Views: ${shortNumber(
+                  data.statistics.viewCount
+                )}</span>
+            </div>
+        </span>`;
+  } else {
+    counts.innerHTML = `Error: ${data.message}`;
+  }
+}
+
+function updateImages(url) {
+  channelProfileImage.style.backgroundImage = `url(${url})`;
 }
 
 /**
  * Verify the value and get the data
- * @param { string } key when enter pressed call the function to get data 
+ * @param { string } key when enter pressed call the function to get data
  */
 async function getData(key) {
-    if (key === "Enter" && input.value !== (null || "" || " ")) {
-        await fetchDataByUsername(input.value.split(" ").join(""));
-        input.value = "";
-    }
+  if (key === "Enter" && input.value !== (null || "" || " ")) {
+    await fetchDataByUsername(input.value.split(" ").join(""));
+    input.value = "";
+  }
 }
 
 /**
- * Converts the numbers into 
+ * Converts the numbers into
  * @param { number } labelValue the value you to short in M OR K
  */
 function shortNumber(labelValue) {
-
-    // Nine Zeroes for Billions
-    return Math.abs(Number(labelValue)) >= 1.0e+9
-
-        ? (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2) + "B"
-        // Six Zeroes for Millions 
-        : Math.abs(Number(labelValue)) >= 1.0e+6
-
-            ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(1) + "M"
-            // Three Zeroes for Thousands
-            : (Math.abs(Number(labelValue))) >= 1.0e+3
-
-                ? Math.abs(Number(labelValue)) / 1.0e+3 + "K"
-
-                : Math.abs(Number(labelValue));
-
+  // Nine Zeroes for Billions
+  return Math.abs(Number(labelValue)) >= 1.0e9
+    ? (Math.abs(Number(labelValue)) / 1.0e9).toFixed(1) + "B"
+    : // Six Zeroes for Millions
+    Math.abs(Number(labelValue)) >= 1.0e6
+    ? (Math.abs(Number(labelValue)) / 1.0e6).toFixed(1) + "M"
+    : // Three Zeroes for Thousands
+    Math.abs(Number(labelValue)) >= 1.0e3
+    ? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(1) + "K"
+    : Math.abs(Number(labelValue));
 }
 
 // Event Listeners for submit events
-document.addEventListener('keyup', (event) => getData(event.key));
-// button.addEventListener('click', () => getData("Enter"));
+document.addEventListener("keyup", (event) => getData(event.key));
 
 // function calls on page load
 fetchDataByUsername();
